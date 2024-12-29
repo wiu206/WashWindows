249198a6461d7626837caeb45b7215596e35a03a
CHENGXee <411630394@o365.tku.edu.tw>
2024-12-30 01:12:53 +0800

    新增草稿

--- Frontend/src/enum/api.ts
+++ Frontend/src/enum/api.ts
@@ -1,3 +1,2 @@
 export enum api{
-    findAll = "http://163.13.201.151.:8877/api/v1/user/findAll"
 }
--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './style/index.css'
 import { RouterProvider } from 'react-router/dom'
 import { router } from './router/router'
 
