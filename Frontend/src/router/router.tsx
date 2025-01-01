c2471c5d49c86f6b06243b134fea588f79714b53
CHENGXee <411630394@o365.tku.edu.tw>
2025-01-02 02:33:04 +0800

    Update for login and register

--- Frontend/src/router/router.tsx
+++ Frontend/src/router/router.tsx
@@ -1,9 +1,19 @@
 import { createHashRouter } from "react-router";
 import  WashWindowsGame from "../view/index";
+import Login from "../view/Login";
+import Register from "../view/Register";
 
 export const router = createHashRouter([
     {
         path: "/",
         element: <WashWindowsGame />,
+    },
+    {
+        path: "/Login",
+        element: <Login />
+    },
+    {
+        path: "/Register",
+        element: <Register />
     },
 ])
