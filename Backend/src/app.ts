2d29c784ce5b2832fad4c313f56ece4e1223d2f8
kevin083177 <kevin083177@gmail.com>
2025-01-05 00:30:58 +0800

    use srv

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -12,7 +12,6 @@ export const DB = new MongoDB({
   name:process.env.DBUSER as string,
   password:process.env.DBPASSWORD as string,
   host:process.env.DBHOST as string,
-  port:process.env.DBPORT as string,
   dbName:process.env.DBNAME as string
 });
 
