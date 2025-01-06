176b14731ffdaf4d1ff22c1230a65ba7b7974211
kevin083177 <kevin083177@gmail.com>
2025-01-07 02:21:25 +0800

    fetch feature and improve styles

--- Frontend/src/view/index.tsx
+++ Frontend/src/view/index.tsx
@@ -1,51 +1,149 @@
 import React, { useState, useEffect } from 'react';
 import Window from "../assets/window.png";
 import Rag from "../assets/rag.png";
+import Dirty from "../assets/dirty.png";
+import { User } from '../interface/User';
 import '../style/index.css';
+import { asyncGet, asyncPut } from '../utils/fetch';
+import { user_api } from '../enum/api';
+import Header from '../component/Header';
 
-interface Rank {
-  user: string;
-  score: number;
+interface RankItem {
+  username: string;
+  points: number;
 }
 
-interface User {
-  name: string;
-  email: string;
-  role: string; // 新增使用者角色
-}
-
-const WashWindowsGame: React.FC = () => {
-  const [score, setScore] = useState(0);
-  const [currentFrame, setCurrentFrame] = useState(3);
+export const WashWindowsGame: React.FC = () => {
+  const [points, setPoints] = useState<number>(0);
+  const [pointsBuffer, setPointsBuffer] = useState<number>(0);
+  const [isUpdating, setIsUpdating] = useState<boolean>(false);
+  const [updateStatus, setUpdateStatus] = useState<string>("");
   const [isPanelOpen, setIsPanelOpen] = useState(false);
   const [currentKey, setCurrentKey] = useState<string | null>(null);
   const [wrongAttempt, setWrongAttempt] = useState(false);
   const [position, setPosition] = useState({ top: 50, left: 50 });
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-
-  const user: User = {
-    name: 'Green',
-    email: 'user@example.com',
-    role: '一般使用者' // 或者 '管理員'
-  };
-
-  const ranks: Rank[] = [
-    { user: 'User1', score: 10 },
-    { user: 'User2', score: 9 },
-    { user: 'User3', score: 8 }
-  ];
-
+  const [dirtyPosition, setDirtyPosition] = useState({ top: 50, left: 50 });
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [user, setUser] = useState<User | null>(null);
+  const [rank, setRank] = useState<RankItem[]>([]);
+  const [isExiting, setIsExiting] = useState<boolean>(false);
+  const token = localStorage.getItem('token');
+  const savedUser = localStorage.getItem('user');
+  useEffect(() => {
+    if (token && savedUser) {
+      const parsedUser = JSON.parse(savedUser);
+      setIsLoggedIn(true);
+      setUser(parsedUser);
+      setPoints(parsedUser.points);
+      
+      // Initialize pointsBuffer with the saved points
+      setPointsBuffer(parsedUser.points);
+    }
+  }, []);
+  
   const togglePanel = () => {
     setIsPanelOpen(!isPanelOpen);
   };
+  
+  const showRankList = async () => {
+    try {
+      const response = await asyncGet(user_api.getAllPoints);
+      if (response.code === 200) {
+        const rankData: RankItem[] = response.body.map((item: any) => ({
+          username: item.username,
+          points: item.points
+        }));
+        setRank(rankData);
+      }
+    } catch (error) {
+      console.log("failed to fetch rank list");
+    }
+  };
 
-  const toggleDropdown = () => {
-    setIsDropdownOpen(!isDropdownOpen);
+  const throttledUpdatePoints = async () => {
+    if (!token || !user?._id || points === pointsBuffer) {
+      return;
+    }
+
+    setIsUpdating(true);
+    try {
+      const response = await asyncPut(user_api.updatePoints, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        },
+        body: {
+          _id: user?._id,
+          points: points
+        }
+      });
+
+      if (response.status === 200) {
+        setPointsBuffer(points);
+        setUpdateStatus("分數已更新！");
+        const updatedUser = { ...user, points: points };
+        localStorage.setItem('user', JSON.stringify(updatedUser));
+        setUser(updatedUser);
+        
+        setTimeout(() => {
+          setIsExiting(true);
+          setTimeout(() => {
+            setUpdateStatus("");
+            setIsExiting(false);
+          }, 300);
+        }, 2000);
+      }
+    } catch (error) {
+      console.error('Error updating points:', error);
+      setUpdateStatus("更新失敗，請稍後再試");
+    } finally {
+      setIsUpdating(false);
+    }
+  };
+
+  useEffect(() => {
+    showRankList();
+    const interval = setInterval(() => {
+      if (!isUpdating) {
+        throttledUpdatePoints();
+      }
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [points, isUpdating]);
+
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+    setIsLoggedIn(false);
+    setUser(null);
+    setPoints(0);
+    setPointsBuffer(0);
+  };
+
+  const handleScoreIncrease = () => {
+    setPoints(prev => prev + 1);
+  };
+
+  const generateRandomPosition = (key: string): { top: number, left: number } => {
+    switch (key) {
+      case "ArrowUp":
+        return { top: 18, left: 48 };
+      case "ArrowDown":
+        return { top: 58, left: 48 };
+      case "ArrowLeft":
+        return { top: 38, left: 40 };
+      case "ArrowRight":
+        return { top: 38, left: 54 };
+      default:
+        return { top: 50, left: 50 };
+    }
   };
 
   const generateRandomKey = () => {
     const keys = ["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"];
-    return keys[Math.floor(Math.random() * keys.length)];
+    const randomKey = keys[Math.floor(Math.random() * keys.length)];
+    const newPosition = generateRandomPosition(randomKey);
+    setDirtyPosition(newPosition);
+    return randomKey;
   };
 
   useEffect(() => {
@@ -53,13 +151,13 @@ const WashWindowsGame: React.FC = () => {
   }, []);
 
   const updatePosition = (key: string) => {
-    const step = 5;
+    const step = 8;
     setPosition((prevPosition) => {
       switch (key) {
         case "ArrowUp":
-          return { ...prevPosition, top: Math.max(0, prevPosition.top - step) };
+          return { ...prevPosition, top: Math.max(0, prevPosition.top - step - 10) };
         case "ArrowDown":
-          return { ...prevPosition, top: Math.min(95, prevPosition.top + step) };
+          return { ...prevPosition, top: Math.min(95, prevPosition.top + step + 10) };
         case "ArrowLeft":
           return { ...prevPosition, left: Math.max(0, prevPosition.left - step) };
         case "ArrowRight":
@@ -77,7 +175,7 @@ const WashWindowsGame: React.FC = () => {
   const handleKeyDown = (event: KeyboardEvent) => {
     const key = event.key;
     if (key === currentKey) {
-      setScore((prevScore) => prevScore + 1);
+      handleScoreIncrease(); // 調用增加分數函式
       setCurrentKey(generateRandomKey());
       setWrongAttempt(false);
       updatePosition(key);
@@ -95,54 +193,32 @@ const WashWindowsGame: React.FC = () => {
 
   return (
     <div className="container">
-      <header className="header">
-        <a className='btn' href='#/'>Washwindows Game</a>
-        <div className="button-group">
-          <a className='button' href='#/Login'>登入</a>
-          <a className='button' href='#/Register'>註冊</a>
-          <div className="user-info" onClick={toggleDropdown} style={{ cursor: 'pointer', position: 'relative' }}>
-            Hi, {user.name}
-            <div className={`dropdown ${isDropdownOpen ? 'open' : ''}`} style={{ display: isDropdownOpen ? 'block' : 'none', position: 'absolute', top: '100%', right: 0, backgroundColor: 'white', border: '1px solid #ccc', borderRadius: '4px', padding: '10px', zIndex: 1000 }}>
-              <p>名稱: {user.name}</p>
-              <p>電子郵件: {user.email}</p>
-              <p>角色: {user.role}</p>
-              <a href="#/Personal">個人資料</a>
-              {user.role === '管理員' && (
-                <>
-                  <a href="#/dashboard">管理員</a>
-                  <a href="#/Manager">管理員選項</a>
-                </>
-              )}
-              <a href="#/">登出</a>
-            </div>
-          </div>
-        </div>
-      </header>
+      <Header isLoggedIn={isLoggedIn} user={user} onLogout={handleLogout} />
 
       <main className="main">
         <div className="game-area">
-          <div>
-            <h2>Score: {score}</h2>
-          </div>
+          <h2 style={{top: 100}}>Score: {points}</h2>
 
           <img className="window" src={Window} alt="Window" />
 
           <div
             className="rag"
             style={{
-              position: 'absolute',
               top: `${position.top}%`,
               left: `${position.left}%`,
-              transform: 'translate(-50%, -50%)',
             }}
           >
             <img className="rag" src={Rag} alt="rag" />
           </div>
-
-          <div className="frame-counter">
-            Frame {currentFrame}
+          <div
+            className="dirty"
+            style={{
+              top: `${dirtyPosition.top}%`,
+              left: `${dirtyPosition.left}%`,
+            }}
+          >
+            <img src={Dirty} alt="dirty" />
           </div>
-
           <div className="controls">
             {["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"].map((key) => (
               <button
@@ -156,6 +232,11 @@ const WashWindowsGame: React.FC = () => {
               </button>
             ))}
           </div>
+          {updateStatus && (
+            <div className={`update-status ${isExiting ? 'exit' : ''}`}>
+              {updateStatus}
+            </div>
+          )}
         </div>
 
         <button
@@ -166,11 +247,17 @@ const WashWindowsGame: React.FC = () => {
         </button>
 
         <div className={`leaderboard ${isPanelOpen ? 'open' : ''}`}>
-          <h3>Rank</h3>
-          {ranks.map((rank, index) => (
+          <h3 style={{textAlign: "center"}}>擦窗戶排行榜</h3>
+          <div className="rank-item">
+            <span className="rank-number">排名</span>
+            <span className="username">清潔工</span>
+            <span className="points">擦窗數</span>
+          </div>
+          {rank.map((item, index) => (
             <div key={index} className="rank-item">
-              <span>{rank.user}:</span>
-              <span>{rank.score}</span>
+              <span className="rank-number">{index + 1}</span>
+              <span className="username">{item.username}</span>
+              <span className="points">{item.points}</span>
             </div>
           ))}
         </div>
@@ -178,5 +265,3 @@ const WashWindowsGame: React.FC = () => {
     </div>
   );
 };
-
-export default WashWindowsGame;
