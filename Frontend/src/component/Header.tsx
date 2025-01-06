1d4ef0601b8623d1fc1719ec834603be5591c610
kevin083177 <kevin083177@gmail.com>
2025-01-07 02:19:19 +0800

    Componentize Header

---
+++ Frontend/src/component/Header.tsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import { User } from '../interface/User';
+import '../style/Header.css';
+
+interface HeaderProps {
+  isLoggedIn: boolean;
+  user: User | null;
+  onLogout: () => void;
+}
+
+const Header: React.FC<HeaderProps> = ({ isLoggedIn, user, onLogout }) => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
+  return (
+    <header className="header">
+      <a className='btn' href='#/'>Washwindows Game</a>
+      <div className="button-group">
+        {!isLoggedIn ? (
+          <>
+            <a className='button' href='#/Login'>登入</a>
+            <a className='button' href='#/Register'>註冊</a>
+          </>
+        ) : (
+          <div className="user-info" onClick={toggleDropdown}>
+            Hi, {user?.username}
+            <div className={`dropdown ${isDropdownOpen ? 'open' : ''}`}>
+              <a href="#/Profile">個人資料</a>
+              <a href='#/Rank'>排行榜</a>
+              {user?.userRole === 'admin' && (
+                <>
+                  <a href="#/Manager">管理員選項</a>
+                </>
+              )}
+              <button onClick={onLogout}>登出</button>
+            </div>
+          </div>
+        )}
+      </div>
+    </header>
+  );
+};
+
+export default Header;
