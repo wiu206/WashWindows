6f2d4e209f20f8b6913d12b65a4938a62a6a4013
kevin083177 <kevin083177@gmail.com>
2025-01-07 23:09:28 +0800

    Update fetch.ts

--- Frontend/src/utils/fetch.ts
+++ Frontend/src/utils/fetch.ts
@@ -4,9 +4,15 @@ const api_base = import.meta.env.VITE_API_URL;
  * @param api 要呼叫的api
  * @returns json 結果
  */
-export async function asyncGet(api: string):Promise<any>{
+export async function asyncGet(api: string, { headers = {} }: {headers?: HeadersInit}):Promise<any>{
     try {
-        const res: Response = await fetch(api)
+        const res: Response = await fetch(api, {
+            headers: {
+                'Access-Control-Allow-Origin': api_base,
+                'Content-Type': 'application/json',
+                ...headers,
+            },
+        })
         try {
             return await res.json()
         } catch (error) {
@@ -37,10 +43,30 @@ export async function asyncPost(api: string, { body, headers = {} }: { body: any
     }
 }
 
-export async function asyncPut(api: string, { body, headers = {} }: { body: any, headers?: HeadersInit }): Promise<Response> {
+export async function asyncPut(api: string, { body, headers = {} }: { body?: any, headers?: HeadersInit }): Promise<Response> {
     try {
         const res: Response = await fetch(api, {
             method: 'PUT',
+            headers: {
+                'Access-Control-Allow-Origin': api_base,
+                'Content-Type': 'application/json',
+                ...headers,
+            },
+            body: body instanceof FormData ? body : JSON.stringify(body),
+            mode: 'cors',
+        });
+        
+        return res;
+    } catch (error) {
+        console.error('Request failed:', error);
+        throw error;
+    }
+}
+
+export async function asyncDelete(api: string, { body, headers = {} }: { body?: any, headers?: HeadersInit }): Promise<Response> {
+    try {
+        const res: Response = await fetch(api, {
+            method: 'DELETE',
             headers: {
                 'Access-Control-Allow-Origin': api_base,
                 'Content-Type': 'application/json',
